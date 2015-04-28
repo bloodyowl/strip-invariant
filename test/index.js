bd242f2ca67992f8622081c761a95f090e9dab15
Matthias Le Brun <mlbli@me.com>
2015-04-28 18:12:44 +0200

    fixed dumb tests

--- test/index.js
+++ test/index.js
@@ -5,8 +5,10 @@ var fs = require("fs")
 
 tape("invariant", function(test) {
   var file = fs.readFileSync(path.resolve(__dirname, "./source.fixture.js"))
-  var transformed = babel.transform(file)
-  test.notEqual("invariant(condition)", -1)
-  test.notEqual("invariant(true === false)", -1)
+  var transformed = babel.transform(file, {
+    plugins: "..",
+  }).code
+  test.notEqual(transformed.indexOf("invariant(condition)"), -1)
+  test.notEqual(transformed.indexOf("invariant(true === false)"), -1)
   test.end()
 })
